08a14f0ed8b56ac0f397d7c91045393eac0de676
Tomasz Baniak <thomas.baniak@gmail.com>
2022-07-16 00:03:51 +0200

    Added jwt generation returned by a temporary endpoint

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,8 +1,18 @@
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import jwt.TokenService
 import mu.KotlinLogging
+import java.time.Duration
 
 private val logger = KotlinLogging.logger {}
 fun main(args: Array<String>) {
-    val server = Server()
+    val tokenService = TokenService(
+        Keys.keyPairFor(SignatureAlgorithm.RS256),
+        Duration.ofSeconds(10)
+    )
+    val server = Server(
+        tokenService = tokenService
+    )
     server.start()
 
     Runtime.getRuntime().addShutdownHook(Thread {
--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,15 +1,16 @@
 import api.HealthEndpoint
+import api.TokenEndpoint
 import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider
 import io.muserver.HttpsConfigBuilder
 import io.muserver.MuServer
 import io.muserver.MuServerBuilder.muServer
 import io.muserver.rest.RestHandlerBuilder
+import jwt.TokenService
 import mu.KotlinLogging
 
 
-class Server {
+class Server(private val tokenService: TokenService) {
 
     companion object {
         private val logger = KotlinLogging.logger {}
@@ -23,13 +24,13 @@ class Server {
             .withHttpsConfig(HttpsConfigBuilder.unsignedLocalhost())
             .addHandler(
                 RestHandlerBuilder.restHandler(
+                    TokenEndpoint(tokenService),
                     HealthEndpoint()
                 )
                     .addCustomReader(JacksonJaxbJsonProvider().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false))
                     .addCustomWriter(JacksonJaxbJsonProvider())
             )
             .start()
-
         logger.info("Server started: ${server.httpsUri()}")
     }
 
